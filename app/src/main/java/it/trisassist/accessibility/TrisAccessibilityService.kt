8126317a14ec403e1cd6bb3bcec4c31f94021922
divadesign-prog <d.iva_design@hotmail.com>
2026-09-14 20:56:45 +0200

    Enable faster reliable tile taps

--- app/src/main/java/it/trisassist/accessibility/TrisAccessibilityService.kt
+++ app/src/main/java/it/trisassist/accessibility/TrisAccessibilityService.kt
@@ -31,11 +31,11 @@ class TrisAccessibilityService : AccessibilityService() {
         val point = points[index]
         val path = Path().apply { moveTo(point.x, point.y) }
         val gesture = GestureDescription.Builder()
-            .addStroke(GestureDescription.StrokeDescription(path, 0, 35))
+            .addStroke(GestureDescription.StrokeDescription(path, 0, 30))
             .build()
         dispatchGesture(gesture, object : GestureResultCallback() {
             override fun onCompleted(gestureDescription: GestureDescription?) {
-                handler.postDelayed({ tapSequence(points, index + 1, finished) }, 130L)
+                handler.postDelayed({ tapSequence(points, index + 1, finished) }, 90L)
             }
             override fun onCancelled(gestureDescription: GestureDescription?) {
                 finished()
