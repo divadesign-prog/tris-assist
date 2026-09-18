9df1aed482020cde61133dd33af7cbe24ca35f2b
divadesign-prog <d.iva_design@hotmail.com>
2026-09-18 21:49:59 +0200

    Prepara Tris Assist v0.26 adattiva

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "it.trisassist"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.25.0"
+        versionCode = 26
+        versionName = "0.26.0"
     }
 
     buildTypes {
