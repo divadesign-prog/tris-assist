9b576107da17972591697ad8462eebbac8546562
divadesign-prog <d.iva_design@hotmail.com>
2026-09-19 18:39:58 +0200

    Prepara Tris Assist v0.27 vassoio sicuro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "it.trisassist"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "0.26.0"
+        versionCode = 27
+        versionName = "0.27.0"
     }
 
     buildTypes {
